208221aa6fc22f4b8005fea36517a6a8b9f26da5
maria <maocampog1@eafit.edu.co>
2024-05-08 08:41:01 -0500

    Esta es la solucion de la Tarea

---
+++ Lenguajes de Programacion/listas/cmake-build-listas/CMakeFiles/listicas.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/maria/OneDrive/Escritorio/Lenguajes de Programacion/listas/Lista.cpp" "CMakeFiles/listicas.dir/Lista.cpp.obj" "gcc" "CMakeFiles/listicas.dir/Lista.cpp.obj.d"
+  "C:/Users/maria/OneDrive/Escritorio/Lenguajes de Programacion/listas/Nodo.cpp" "CMakeFiles/listicas.dir/Nodo.cpp.obj" "gcc" "CMakeFiles/listicas.dir/Nodo.cpp.obj.d"
+  "C:/Users/maria/OneDrive/Escritorio/Lenguajes de Programacion/listas/main.cpp" "CMakeFiles/listicas.dir/main.cpp.obj" "gcc" "CMakeFiles/listicas.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lenguajes de Programacion/listas/cmake-build-listas/CMakeFiles/listicas.dir/Nodo.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/listicas.dir/Nodo.cpp.obj: \
+ C:\Users\maria\OneDrive\Escritorio\Lenguajes\ de\ Programacion\listas\Nodo.cpp \
+ C:\Users\maria\OneDrive\Escritorio\Lenguajes\ de\ Programacion\listas\Nodo.h
